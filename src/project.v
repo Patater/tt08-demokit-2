Patater/tt08-demokit

Update project description

src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_patater_demokit (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path

test/tb.v
@@ -23,8 +23,7 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_patater_demokit user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST

Patater/tt08-demokit-2

src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_patater_demokit_2 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path

test/tb.v
@@ -23,8 +23,7 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_patater_demokit_2 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST